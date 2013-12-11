a3fd0c14c118a1b2e4a82e4dfaaf0bc19774ad7a
Shreyas <skraman1999@gmail.com>
2013-12-11 15:17:12 -0800

    First commit.

---
+++ src/game/SlickGame.java
@@ -0,0 +1,38 @@
+package game;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class SlickGame extends BasicGame {
+
+    public SlickGame() {
+        super("Slick Game");
+    }
+
+    public static void main(String[] arguments) {
+        try {
+            AppGameContainer app = new AppGameContainer(new SlickGame());
+            app.setDisplayMode(600, 480, false);
+            app.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void init(GameContainer container) throws SlickException {
+        
+    }
+
+    @Override
+    public void update(GameContainer container, int delta) throws SlickException {
+        
+    }
+
+    public void render(GameContainer container, Graphics g) throws SlickException {
+        
+    }
+}
