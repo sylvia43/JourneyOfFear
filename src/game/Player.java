a0bb451a0e36b7a792d471cf68691b354e1e36dc
Shreyas <skraman1999@gmail.com>
2013-12-19 18:51:08 -0800

    Got mouse direction to work.

--- src/game/Player.java
+++ src/game/Player.java
@@ -1,5 +1,6 @@
 package game;
 
+import org.lwjgl.input.Mouse;
 import org.newdawn.slick.Animation;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
@@ -26,6 +27,11 @@ public class Player {
     private static boolean _leftPressed;
     private static boolean _rightPressed;
     
+    private static int _direction;
+    
+    private static boolean attacking;
+    private static int attackTimer;
+    
     public static void init(GameContainer container, Options options) throws SlickException {
         player_down = ResourceLoader.initializeAnimation("player_forward.png",4,166);
         player_up = ResourceLoader.initializeAnimation("player_backward.png",4,166);
@@ -36,6 +42,7 @@ public class Player {
     }
     public static void update(GameContainer container, int delta) {
         movePlayer(container.getInput(), delta);
+        resolveAttack(container.getInput(), delta, container.getHeight());
     }
     public static void render(GameContainer container, Graphics g) throws SlickException {
         Animation player_sprite = null;
@@ -53,7 +60,8 @@ public class Player {
                 player_sprite = player_right;
                 break;
         }
-        if (SlickGame.DEBUG_MODE)
+        Input input = container.getInput();
+        if (SlickGame.DEBUG_MODE) {
             g.drawString((_downHeld?"dh ":"")
                     + (_upHeld?"uh ":"")
                     + (_leftHeld?"lh ":"")
@@ -62,10 +70,56 @@ public class Player {
                     + (_upPressed?"up ":"")
                     + (_leftPressed?"lp ":"")
                     + (_rightPressed?"rp ":""), 50, 50);
-        player_sprite.draw((int)player_x,(int)player_y);
+            g.drawString(String.valueOf(_direction), 50, 75);
+            g.drawString("x: " + String.valueOf(player_x), 50, 100);
+            g.drawString("y: " + String.valueOf(player_y), 50, 125);
+            g.drawString("mx: " + Mouse.getX(), 50, 150);
+            g.drawString("my: " + Mouse.getY(), 50, 175);
+            g.drawString("dx: " + String.valueOf(Mouse.getX()-player_x), 50, 200);
+            g.drawString("dy: " + String.valueOf(Mouse.getY()+player_y-container.getHeight()), 50, 225);
+
+        }
+        player_sprite.draw((int)player_x-32,(int)player_y-32);
     }
     
-    public static void movePlayer(Input input,int delta) {
+    public static void resolveAttack(Input input, int delta, int height) {
+        if (/*input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON) && */!attacking) {
+            double dy = player_y+Mouse.getY()-height;
+            double dx = Mouse.getX()-player_x;
+            int direction;
+            if (dx>=0) {
+                if (dy>0) {
+                    if (dx>=dy)
+                        direction = _direction = 0;
+                    else
+                        direction = _direction = 1;
+                }
+                else {
+                    if (dx>=-dy)
+                        direction = _direction = 7;
+                    else
+                        direction = _direction = 6;
+                }
+            } else {
+                if (dy>0) {
+                    if (-dx>=dy)
+                        direction = _direction = 3;
+                    else
+                        direction = _direction = 2;
+                }
+                else {
+                    if (-dx>=-dy)
+                        direction = _direction = 4;
+                    else
+                        direction = _direction = 5;
+                }
+            }
+            //attack(direction);
+            //int direction = _direction = (int) (8 * Math.atan2((player_y+Mouse.getY()-height),(Mouse.getX()-player_x))/Math.PI);
+        }
+    }
+    
+    public static void movePlayer(Input input, int delta) {
         boolean downHeld = _downHeld = input.isKeyDown(keybind.KEY_DOWN);
         boolean downPressed = _downPressed  = input.isKeyPressed(keybind.KEY_DOWN);
         boolean upHeld = _upHeld  = input.isKeyDown(keybind.KEY_UP);
