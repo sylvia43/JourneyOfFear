b9f70dce6caac55042027ea717fa657039c40dde
Shreyas <skraman1999@gmail.com>
2014-01-01 22:32:23 -0800

    Did simplification for ImageMask.

--- src/game/ImageMask.java
+++ src/game/ImageMask.java
@@ -18,24 +18,29 @@ public class ImageMask {
             }
         }
         
-        boolean[][] newMask = startMask;
-        /* Broken code to simplify the mask.
-        for (int i=1;i<width-1;i++) {
-            for (int j=1;j<height-1;j++) {
-                if (startMask[i][j]
-                        && (i<16||startMask[i+1][j])
-                        && (i<16&&j>0||startMask[i+1][j-1])
-                        && (i<16&&j<16||startMask[i+1][j+1])
-                        && (j<16||startMask[i][j+1])
-                        && (i>0&&j<16||startMask[i-1][j+1])
-                        && (i>0||startMask[i-1][j])
-                        && (i>0&&j>0||startMask[i-1][j-1])
-                        && (j>0||startMask[i][j-1]))
-                    newMask[i][j] = false;
+        boolean[][] simplifiedMask = new boolean[width][height];
+        
+        for (int i=0;i<width;i++) {
+            for(int j=0;j<height;j++) {
+                simplifiedMask[i][j] = startMask[i][j];
             }
         }
-        */
-        mask = newMask;
+                
+        for (int i=1;i<width-1;i++) {
+            for (int j=1;j<height-1;j++) {
+                simplifiedMask[i][j] = startMask[i][j]
+                        && !((startMask[i+1][j])
+                        && (startMask[i+1][j-1])
+                        && (startMask[i+1][j+1])
+                        && (startMask[i][j+1])
+                        && (startMask[i-1][j+1])
+                        && (startMask[i-1][j])
+                        && (startMask[i-1][j-1])
+                        && (startMask[i][j-1]));
+            }
+        }
+        mask = simplifiedMask;
+        System.out.println(this);
     }
     
     public boolean[][] getMask() { return mask; }
@@ -68,7 +73,7 @@ public class ImageMask {
         String s = "";
         for (int i=0;i<array.length;i++) {
             for (int j=0;j<array[i].length;j++) {
-                s += array[i][j]?"1":"0";
+                s += array[j][i]?"X":" ";
             }
             s+="\n";
         }
