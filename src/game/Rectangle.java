346dc6f1e76fcf8b315af40fb67a2f2f0da2f152
Shreyas <skraman1999@gmail.com>
2014-01-01 22:18:47 -0800

    Look at that sexy intersects method. Look at it.

--- src/game/Rectangle.java
+++ src/game/Rectangle.java
@@ -3,6 +3,9 @@ package game;
 public class Rectangle {
     private double x1,x2,y1,y2;
     
+    public double getX() { return x1; }
+    public double getY() { return y1; }
+    
     public Rectangle(double x1, double y1, double x2, double y2) {
         this.x1 = x1;
         this.x2 = x2;
@@ -10,6 +13,7 @@ public class Rectangle {
         this.y2 = y2;
     }
     
-    public double getX() { return x1; }
-    public double getY() { return y1; }
+    public boolean intersects(Rectangle other) {
+        return x1<=other.x2 && x2>=other.x1 && y1<=other.y2 && y2>=other.y1;
+    }
 }
