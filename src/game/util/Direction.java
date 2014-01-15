fcc42e9011fe5efdf2f36442f74007eac6cae661
Shreyas <skraman1999@gmail.com>
2014-01-14 17:56:27 -0800

    Tested; it works, renamed to direction, added 8 sides.

---
+++ src/game/util/Direction.java
@@ -0,0 +1,15 @@
+package game.util;
+
+public class Direction {
+    
+    public static Direction right = new Direction();
+    public static Direction upright = new Direction();
+    public static Direction up = new Direction();
+    public static Direction upleft = new Direction();
+    public static Direction left = new Direction();
+    public static Direction downleft = new Direction();
+    public static Direction down = new Direction();
+    public static Direction downright = new Direction();
+    
+    private Direction() { }
+}
