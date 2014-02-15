99fd61ab4bbe01d64bf49e3f136c209cc29d9dea
Brennan <Brennan@Brennans-PC>
2014-02-15 13:26:22 -0800

    Working on spikes

--- src/game/environment/Hazards.java
+++ src/game/environment/Hazards.java
@@ -12,25 +12,83 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 
 public class Hazards {
+    
     protected EntitySprite sprite;
     protected String spritepath;
     protected int spritePointer;
-    protected int animationSpeed;
+    protected int animationSpeed;  
     protected boolean collisionHit;
     
     protected int x;
-    protected int y;
-    
-    protected Player player;
-    
-    public Hazards(Player player) {
-        this.player = player;
-    }
-    
-    //getters
+    protected int y;    
+        
+    //Getters. These methods probably can be left alone.
     public int getX() { return x; }
     public int getY() { return y; }
     
-    //these should be overriden
-    protected void testCollision() {}
+    public ImageMask getCollisionMask() {
+        return sprite.getAnimationMask(spritePointer)
+                .getImageMask(sprite.getAnim(spritePointer).getFrame());
+    }
+    
+    // By default enemies don't have attacks.
+    public Rectangle getAttackMask() { return null; }
+    
+    public Hazards() {
+       
+    }
+    
+    public void setX(int x) { this.x = x; }
+    public void setY(int y) { this.y = y; }
+    
+    //Game loop methods
+    public void init(GameContainer container) throws SlickException {
+        initializeVariables();
+        initializeSprite();
+       
+    }
+    
+    public void update(GameContainer container, int delta) {
+       
+        resolveCollision();
+       
+    }
+    
+    public void render(GameContainer container, Graphics g) throws SlickException {
+        sprite.getAnim(spritePointer).draw(x,y,64,64);
+       
+        if (StatePlaying.DEBUG_MODE)
+            renderDebugInfo(g);
+    }
+    
+    // Miscelleneous universal methods.    
+    protected void renderDebugInfo(Graphics g) {
+        g.setColor(Color.white);
+        g.drawString("x: " + String.valueOf(x),10+x+64,38+y+64);
+        g.drawString("y: " + String.valueOf(y),10+x+64,52+y+64);
+    }
+    
+    
+    //Empty methods. These methods should be overriden
+    protected void initializeVariables() { }
+    protected void initializeSprite() throws SlickException { initializeMask(); }
+    protected void resolveCollision() { }
+    
+    //Other methods. These can be overriden if necessary.
+    protected void initializeMask() throws SlickException {
+        sprite.setMasks(
+                createMask(0),
+                createMask(1),
+                createMask(2),
+                createMask(3)
+        );
+    }
+    
+    protected AnimationMask createMask(int index) {
+        ImageMask[] masks = new ImageMask[4];
+        for (int i=0;i<4;i++) {
+            masks[i] = new ImageMask(sprite.getAnim(index).getImage(i));
+        }
+        return new AnimationMask(masks);
+    }
 }
--- src/game/environment/Spikes.java
+++ src/game/environment/Spikes.java
@@ -1,11 +1,12 @@
 package game.environment;
 
 import game.player.Player;
-import game.sprite.AnimationMask;
 import game.sprite.EntitySprite;
-import game.sprite.ImageMask;
-import game.sprite.Rectangle;
 import game.state.StatePlaying;
+import game.util.resource.AnimationLibrary;
+import game.util.resource.SoundLibrary;
+import game.util.resource.SoundPlayer;
+import org.newdawn.slick.Animation;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
@@ -13,20 +14,50 @@ import org.newdawn.slick.SlickException;
 
 public class Spikes extends Hazards {
     
-    public Spikes(Player player){
-        super(player);
+ protected Player player;
+
+    public Spikes(Player player) {
+        super();
+        this.player=player;
+        this.x=(int)(Math.random()*StatePlaying.WORLD_SIZE_X);
+        this.y=(int)(Math.random()*StatePlaying.WORLD_SIZE_Y);
+        this.animationSpeed = 500;
     }
     
+    @Override
+    protected void initializeVariables() {
+        spritePointer = 3;
+    }
     
-    protected void testCollision(){
-        collisionHit = player.getCollisionMask().intersects(getCollisionMask(),player.getX(),player.getY());
-        if (collisionHit)
+    @Override
+    protected void initializeSprite() throws SlickException {
+        sprite = new EntitySprite(4);
+        Animation[] animList = {
+            AnimationLibrary.SPIKES.getAnim(),
+           
+        };
+        sprite.setAnimations(animList);
+        initializeMask();
+    }
+    
+ 
+    @Override
+    protected void resolveCollision() {
+        boolean isHit = getCollisionMask()
+                .intersects(player.getCollisionMask(),x,y,player.getX(),player.getY());
+        if (isHit)
             player.resolveHit(x,y);
     }
-    public Rectangle getCollisionMask() {
+ 
+    
+    @Override
+    protected void renderDebugInfo(Graphics g) {
+        g.setColor(Color.white);
+        g.drawString("x: " + String.valueOf(x),10+x+64,38+y+64);
+        g.drawString("y: " + String.valueOf(y),10+x+64,52+y+64);
         
-        return new Rectangle(x,y,x+64,y+64);
-        
+        if (StatePlaying.DEBUG_COLLISION) {
+            getCollisionMask().draw(x,y,g);
+        }
     }
 }
-
