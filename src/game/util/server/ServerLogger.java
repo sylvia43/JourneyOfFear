ceace0670d8deb728537cd7a1a7786f21eeda2cc
Shreyas <skraman1999@gmail.com>
2014-02-19 20:35:20 -0800

    Oops forgot to add ServerLogger.

---
+++ src/game/util/server/ServerLogger.java
@@ -0,0 +1,10 @@
+package game.util.server;
+
+public class ServerLogger {
+    
+    private ServerLogger() { }
+    
+    public static void log(String message) {
+        System.out.println(message);
+    }
+}
