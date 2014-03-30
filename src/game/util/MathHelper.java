5319f163b7c35757e879c9ba378f728e3ac3ce78
Shreyas <skraman1999@gmail.com>
2014-03-29 23:56:51 -0700

    Comments are for scrubs. This method is so obvious (actually though by name).

--- src/game/util/MathHelper.java
+++ src/game/util/MathHelper.java
@@ -2,10 +2,6 @@ package game.util;
 
 public class MathHelper {
     
-    /**
-     * Finds the median of three numbers.
-     * @return The median of a, b, and c.
-     */
     public static double median(double a, double b, double c) {
         return (a<=b)?((b<=c)?b:((a<c)?c:a)):((a<=c)?a:((b<c)?c:b));
     }
