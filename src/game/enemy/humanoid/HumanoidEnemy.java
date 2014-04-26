00ac03062b34889042bba00ca30e30e0d4f1b2b2
QuantumPhi <phi.quantum@gmail.com>
2014-04-25 17:41:01 -0700

    Humanoid enemy framework (See game.enemy.humanoid)

---
+++ src/game/enemy/humanoid/HumanoidEnemy.java
@@ -0,0 +1,11 @@
+package game.enemy.humanoid;
+
+import game.enemy.SmartEnemy;
+import game.player.Player;
+
+abstract class HumanoidEnemy extends SmartEnemy {
+    
+    public HumanoidEnemy(Player player) { super(player); }
+    
+    
+}
---
+++ src/game/enemy/humanoid/Rogue.java
@@ -0,0 +1,38 @@
+package game.enemy.humanoid;
+
+import game.player.Player;
+
+public class Rogue extends HumanoidEnemy {
+    
+    public Rogue(Player player) { super(player); }
+
+    @Override
+    protected void initializeAttack() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void resolveAttack(int delta) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void renderAttack() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void initializeSprite() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void move(int delta) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void resolveHit(int ox, int oy, int attackId, int damage, double mult) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
