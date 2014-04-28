735c78eb98531a9313f78e9eba0aab2932c75603
Shreyas <skraman1999@gmail.com>
2014-04-28 14:44:04 -0700

    Tiled map something.

--- src/game/map/TiledMap.java
+++ src/game/map/TiledMap.java
@@ -1,5 +1,7 @@
 package game.map;
 
+import java.util.Random;
+
 public class TiledMap {
     
     private Tile[][] map;
@@ -20,72 +22,38 @@ public class TiledMap {
     }
     
     public void init() {
-        if (Math.random()<0.5)
-            fillStandardGrass();
-        else
-            fillStandardCobble();
+        fillStandardGrass();
         
-        //for (int i=0;i<20;i++)
-        //    createGrassPatch();
+        generateRoad();
     }
-    
-    private void createGrassPatch() {
-        int size = (int)(Math.random()*3);
         
-        int x = (int)(Math.random()*(width-2*size)+size);
-        int y = (int)(Math.random()*(height-2*size)+size);
-        
-        for (int i=-size;i<=size;i++) {
-            for (int j=-size;j<=size;j++) {
-                if (i>-size && j>-size && i<size && j<size) {
-                    map[x+i][y+j] = Tile.GRASS_BASIC;
-                    continue;
-                }
-                if (i>0 && j>0)
-                    map[x+i][y+j] = Tile.GRASS_COBBLE_TRANS_SMALL_BOTTOM_RIGHT;
-                else if (i>0 && j<0)
-                    map[x+i][y+j] = Tile.GRASS_COBBLE_TRANS_SMALL_TOP_RIGHT;
-                else if (i<0 && j>0)
-                    map[x+i][y+j] = Tile.GRASS_COBBLE_TRANS_SMALL_BOTTOM_LEFT;
-                else if (i<0 && j<0)
-                    map[x+i][y+j] = Tile.GRASS_COBBLE_TRANS_SMALL_TOP_LEFT;
-                else if (i>0)
-                    map[x+i][y+j] = Tile.GRASS_COBBLE_TRANS_SMALL_RIGHT;
-                else if (i<0)
-                    map[x+i][y+j] = Tile.GRASS_COBBLE_TRANS_SMALL_LEFT;
-                else if (j>0)
-                    map[x+i][y+j] = Tile.GRASS_COBBLE_TRANS_SMALL_BOTTOM;
-                else if (j<0)
-                    map[x+i][y+j] = Tile.GRASS_COBBLE_TRANS_SMALL_TOP;
-            }
-        }
-    }
-    
-    public void fillStandardGrass() {
+    private void fillStandardGrass() {
         fill(Tile.GRASS_BASIC);
         randomize(Tile.GRASS_VARIANT,0.2);
         randomize(Tile.GRASS_SHIFT,0.1);
         randomize(Tile.GRASS_BOLD,0.05);
         randomize(Tile.GRASS_FLOWER,0.01);
     }
-    
-    public void fillStandardCobble() {
-        fill(Tile.COBBLE_BASIC);
-        randomize(Tile.COBBLE_VARIANT1,0.2);
-        randomize(Tile.COBBLE_VARIANT2,0.1);
-        randomize(Tile.COBBLE_ACCENT_GRASS,0.05);
-    }
-    
+        
     private void fill(Tile tile) {
         for(int i=0;i<map.length;i++)
             for(int j=0;j<map[i].length;j++)
                 map[i][j] = tile;
     }
     
-    public void randomize(Tile tile, double chance) {
+    private void randomize(Tile tile, double chance) {
         for(int i=0;i<map.length;i++)
             for(int j=0;j<map[i].length;j++)
                 if (Math.random()<chance)
                     map[i][j] = tile;
+    }
+    
+    private void generateRoad() {
+        Random r = new Random();
+        
+        int dir = r.nextInt(4);
+        
+        int sx = dir==0||dir==2 ? 0 : width;
+        int oy = dir==1||dir==3 ? 0 : width;
     }
 }
