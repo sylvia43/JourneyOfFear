1a165d210ceeda1265ebf7b3fb226f021e3fc258
QuantumPhi <phi.quantum@gmail.com>
2014-05-07 19:25:24 -0700

    Still tweaking

--- src/game/map/util/Cell.java
+++ src/game/map/util/Cell.java
@@ -5,45 +5,36 @@ import java.util.Arrays;
 public class Cell {
     
     public static void main(String[] args) {
-        int[][] cells_noIter = genCells(5, 5, 0);
-        int[][] cells = genCells(5, 5, 1000);
-        
-        for(int[] cell : cells_noIter)
-            System.out.println(Arrays.toString(cell));
+        int[][] cells = genCells(10, 10, 1000);
         
         System.out.println();
-        
-        for(int[] cell : cells)
-            System.out.println(Arrays.toString(cell));
+        printArray(cells);
     }
     
     public static int[][] genCells(int width, int height, int iterations) {
         int[][] cellBlock = new int[width][height];
         randomize(cellBlock);
+        printArray(cellBlock);
         for(int i = 0; i < iterations; i++)
             iterateCells(cellBlock);
         return cellBlock;
     }
     
     private static void iterateCells(int[][] array) {
-        int valid;
         int count;
         int[][] ptr;
         int[][] temp = new int[array.length][array[0].length];
         for(int i = 0; i < array.length; i++) {
             for(int j = 0; j < array[0].length; j++) {
-                valid = 0;
                 count = 0;
                 ptr = getAdjacentIndices(i, j);
                 for(int[] index : ptr) {
                     if((index[0] < array.length && index[0] >= 0) && (index[1] < array[0].length && index[1] >= 0)) {
-                        valid++;
-                        if(array[index[0]][index[1]] != array[i][j]) {
+                        if(array[index[0]][index[1]] != array[i][j])
                             count++;
-                        }
                     }
                 }
-                temp[i][j] = count <= valid / 2 ? 0 : 1;
+                temp[i][j] = count <= 3 && count >= 2 ? 1 : 0;
             }
         }
         
@@ -69,5 +60,10 @@ public class Cell {
             {index1, index2+1},
             {index1+1, index2+1}
         };
+    }
+    
+    private static void printArray(int[][] array) {
+        for(int[] cell : array)
+            System.out.println(Arrays.toString(cell));
     }
 }
